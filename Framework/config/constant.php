e1cb696906f1d500c0965f4c710bc8cd2451cd6c
Henk <Henk@192.168.2.8>
2015-08-10 14:55:26 +0200

    Begin van MVC Framework erbij gegooit

---
+++ Framework/config/constant.php
@@ -0,0 +1,10 @@
+<?php
+// main folder
+define ('URL', '//');
+
+// database settings
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', '');
+define('DB_USER', 'root');
+define('DB_PASS', '');
