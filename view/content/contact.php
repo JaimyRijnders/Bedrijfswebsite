dacccfdd435016fb5e16d3e32a9d111ee7a81e8e
JaimyRijnders <jaimyrb0@hotmail.com>
2015-09-05 00:12:31 +0200

    Aangekloot met contact

--- view/content/contact.php
+++ view/content/contact.php
@@ -4,30 +4,53 @@
         <h1><p class="stripes">==</p></h1>
 
 
-        
+
         <ul id="contactList">
             <li>
                 <div class="contactThird">
                     <i class="fa fa-question fa-3x"></i>
+                    <hr>
+                    <ul class="contactInfo">
+                        <li class="contactInfoItem" style="text-align: center;">
+                            <p class="contactText" style="margin-left: -35px; margin-top: -5px;">
+                                Benieuwd wat wij voor u of uw <br /> bedrijf kunnen betekenen? Klik hieronder en laat direct een <br /><br /><b><a href="#">Offerte opmaken</a></b>
+                            </p>
+                        </li>
+                    </ul>
                 </div>
             </li>
-            
-            <li>
-                <div class="contactThird">
-                    <i class="fa fa-envelope fa-3x"></i>
-                </div>
-            </li>
-            
+
             <li>
                 <div class="contactThird">
                     <i class="fa fa-phone fa-3x"></i>
+                    <hr>
+                    <ul class="contactInfo" style="text-align: center;">
+                        <li class="contactInfoItem">
+                            <p class="contactText" style="margin-left: -35px; margin-top: -5px;">Mocht u ons telefonisch willen bereiken dan kunt u maandag t/m vrijdag tussen 09:00 en 17:00 <br /> bellen naar
+                                
+                                06 123 456 78
+                            </p>
+                        </li>
+                    </ul>
                 </div>
             </li>
-            
+
+            <li>
+                <div class="contactThird">
+                    <i class="fa fa-comments fa-3x"></i>
+                    <hr>
+                    <ul class="contactInfo">
+                        <li class="contactInfoItem"><i class="fa fa-facebook-official"></i><a href="#">Facebook.com/BlueLotus</a></i></li>
+                        <li class="contactInfoItem"><i class="fa fa-twitter"><a href="#">Twitter.com/BlueLotus</a></i></li>
+                        <li class="contactInfoItem"><i class="fa fa-envelope"><a href="#">info@BlueLotus.nl</a></i></li>
+                    </ul>
+                </div>
+            </li>
+
         </ul>
 
         <p id="disclaimer">&COPY;&nbsp; 2015 Blue Lotus &nbsp;&nbsp; - &nbsp;&nbsp; Alle rechten voorbehouden &nbsp;&nbsp; - &nbsp;&nbsp; <a href='#'>Webmaster</a></p> 
-            
+
     </div>
 
 </div>
